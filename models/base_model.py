6730706498afa373986af1f24ab71b739f55018c
Mafupa <ryankarera@gmail.com>
2024-05-25 18:17:10 +0200

    0x00 Task 2

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base Model Class"""
+import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -11,10 +12,19 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
         """save function"""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """to dict function"""
