8ddc37ecb2b287d498eccafb68ce9583184b8428
Mafupa <ryankarera@gmail.com>
2024-05-25 18:34:06 +0200

    0x00 Task 2 Dummy tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,6 +10,26 @@ class TestBaseModel(unittest.TestCase):
         my_model.name = "Test Model"
         self.assertEqual(my_model.name, "Test Model")
 
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test_two_models_unique_ids(self):
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
+
 
 if __name__ == "__main__":
     unittest.main()
