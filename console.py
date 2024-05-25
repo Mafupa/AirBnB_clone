3d56fec326b6d8786bf13cc64ca13b78988d78f9
Mafupa <ryankarera@gmail.com>
2024-05-25 20:39:06 +0200

    0x00 Task 6

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""console"""
+import cmd
+
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Quit command to exit the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
